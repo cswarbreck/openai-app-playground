72910af6f58078d1d4097016b630d6ca80a4de28
Cosmo Swarbreck <c.swarbreck@gmail.com>
2023-02-03 20:14:06 +0000

    Adds models

---
+++ pages/api/models.ts
@@ -0,0 +1,16 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const response = await openai.listModels();
+  res.status(200).json(response.data);
+}
